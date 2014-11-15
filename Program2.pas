22e57fade8a50c07d970ee3a3d536f46d474b31b
Oleg Shcherbakov <shcherbakov.o@gmail.com>
2014-11-15 17:29:43 +0300

    HEX multiplication table

--- Program2.pas
+++ Program2.pas
@@ -1,5 +1,31 @@
 program a;
 
+function toHexDigit(n : integer) :char;
+var k : integer;
+begin
+if (n in [0..9])
+then
+  k:= ord('0') + n
+ else
+  k:= ord('A') + n - 10;
+toHexDigit:= chr(k);
+end;
+
+function d2h(n :integer) : string;
+var s : string;
+    x : integer;
+begin
+ s := '';
+ while n > 0 do
+   begin
+   x:= n mod 16;
+   n:= n div 16;
+   s:= s + toHexDigit(x);
+   end;
+   
+  d2h := ReverseString(s);
+end;
+
 function d2o(n :integer) : string;
 var s : string;
 begin
@@ -14,10 +40,10 @@ end;
 var i, j : integer;
 begin
 
-for i := 1 to 7 do
+for i := 1 to 15 do
  begin
-  for j := 1 to 7 do
-  write(d2o(i*j) : 3);
+  for j := 1 to 15 do
+  write(d2h(i*j) : 3);
   writeln();
  end;
   
